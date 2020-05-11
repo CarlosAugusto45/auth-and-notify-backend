d6f081682d59e6ce1c6b0fabe07d28d00aa17e41
Carlos Augusto <carlosass0.45@gmail.com>
2020-05-11 09:44:14 -0300

    Atualização na estrutura do backend, adicionando as models e schemas

---
+++ src/database/index.js
@@ -0,0 +1,30 @@
+import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
+
+import databaseConfig from '../config/database';
+
+import User from '../app/models/User';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+    this.mongo();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/authnotify',
+      { userNewUrlParser: true, useFindAndModify: true }
+    );
+  }
+}
+
+export default new Database();
