800bcb1e99af950812b4fb4a4ce40b489a4a9ba9
Carlos Augusto <carlosass0.45@gmail.com>
2020-05-11 09:11:01 -0300

    Estrutura inicial feita e finalizada até o momento

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+
+import Routes from './routes';
+import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+  }
+
+  middlewares() {
+    this.server.use(cors());
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(Routes);
+  }
+}
+
+export default new App().server;
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: '',
+  host: '',
+  username: '',
+  password: '',
+  database: '',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
